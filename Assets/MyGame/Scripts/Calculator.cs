001964e788f937cacd80993d908173292e62b694
MatteoEtzer <matteoetzer@gmx.at>
2021-03-02 09:59:11 +0100

    calculator start

---
+++ Assets/MyGame/Scripts/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+    public InputField Number1;
+    public InputField Number2;
+    public Text Result;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Addition()
+    {
+        Result.text = Number1.text + Number2.text;
+    }
+
+    public void Subtraction()
+    {
+
+    }
+}
--- Assets/MyGame/Scripts/SceneSwitch.cs
+++ Assets/MyGame/Scripts/SceneSwitch.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneSwitch12 : MonoBehaviour
+public class SceneSwitch : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
