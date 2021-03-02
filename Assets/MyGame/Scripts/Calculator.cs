0eadbb97a4b7a66e2d92b048b785047156eeebb5
MatteoEtzer <matteoetzer@gmx.at>
2021-03-02 10:12:20 +0100

    calculator done

--- Assets/MyGame/Scripts/Calculator.cs
+++ Assets/MyGame/Scripts/Calculator.cs
@@ -8,25 +8,22 @@ public class Calculator : MonoBehaviour
     public InputField Number1;
     public InputField Number2;
     public Text Result;
+
+    public int Number1Int;
+    public int Number2Int;
+    public int ResultInt;
     
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-
-    }
-
     public void Addition()
     {
-        Result.text = Number1.text + Number2.text;
+        Number1Int = int.Parse(Number1.text);
+        Number2Int = int.Parse(Number2.text);
+        Result.text = (Number1Int + Number2Int).ToString();
     }
 
     public void Subtraction()
     {
-
+        Number1Int = int.Parse(Number1.text);
+        Number2Int = int.Parse(Number2.text);
+        Result.text = (Number1Int - Number2Int).ToString();
     }
 }
