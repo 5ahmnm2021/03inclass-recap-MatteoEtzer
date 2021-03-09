bfc7d916ad51fcff043c80a93303d1d8425f96bc
MatteoEtzer <matteoetzer@gmx.at>
2021-03-09 10:37:42 +0100

    try-catch start

--- Assets/MyGame/Scripts/Calculator.cs
+++ Assets/MyGame/Scripts/Calculator.cs
@@ -8,35 +8,93 @@ public class Calculator : MonoBehaviour
     public InputField Number1;
     public InputField Number2;
     public Text Result;
+    public Text Message;
 
-    public float Number1Float;
-    public float Number2Float;
+    public float number1Float;
+    public float number2Float;
     
     public void Addition()
     {
-        Number1Float = float.Parse(Number1.text);
-        Number2Float = float.Parse(Number2.text);
-        Result.text = (Number1Float + Number2Float).ToString();
+        try
+        {
+            number1Float = float.Parse(Number1.text);
+        }
+        catch (System.Exception)
+        {
+            Message.text = "Try again with numbers";
+        }
+        try
+        {
+            number2Float = float.Parse(Number2.text);
+
+        }
+        catch (System.Exception)
+        {
+            Message.text = "Try again with numbers";
+        }
+        Result.text = (number1Float + number2Float).ToString();
     }
 
     public void Subtraction()
     {
-        Number1Float = float.Parse(Number1.text);
-        Number2Float = float.Parse(Number2.text);
-        Result.text = (Number1Float - Number2Float).ToString();
+        try
+        {
+            number1Float = float.Parse(Number1.text);
+        }
+        catch (System.Exception ex)
+        {
+
+        }
+        try
+        {
+            number2Float = float.Parse(Number2.text);
+        }
+        catch (System.Exception ex)
+        {
+
+        }
+        Result.text = (number1Float - number2Float).ToString();
     }
 
     public void Multiply()
     {
-        Number1Float = float.Parse(Number1.text);
-        Number2Float = float.Parse(Number2.text);
-        Result.text = (Number1Float * Number2Float).ToString();
+        try
+        {
+            number1Float = float.Parse(Number1.text);
+        }
+        catch (System.Exception ex)
+        {
+
+        }
+        try
+        {
+            number2Float = float.Parse(Number2.text);
+        }
+        catch (System.Exception ex)
+        {
+
+        }
+        Result.text = (number1Float * number2Float).ToString();
     }
 
     public void Divide()
     {
-        Number1Float = float.Parse(Number1.text);
-        Number2Float = float.Parse(Number2.text);
-        Result.text = (Number1Float / Number2Float).ToString();
+        try
+        {
+            number1Float = float.Parse(Number1.text);
+        }
+        catch (System.Exception ex)
+        {
+
+        }
+        try
+        {
+            number2Float = float.Parse(Number2.text);
+        }
+        catch (System.Exception ex)
+        {
+
+        }
+        Result.text = (number1Float / number2Float).ToString();
     }
 }
